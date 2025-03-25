ba5a7bf4129a405a6da4170a0f5fd5d31635ac00
Saisrnivas-s575720 <s575720@nwmissouri.edu>
2025-03-25 15:05:49 -0500

    Final updates before Render deployment

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// Define variables
+let ratio = 0.75;
+let acc = 1000;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  acc = acc * ratio;  // Multiply acc by ratio
+  res.send(`Value is: ${acc.toFixed(2)}`); // Send the updated value with 2 decimal places
 });
 
 module.exports = router;
